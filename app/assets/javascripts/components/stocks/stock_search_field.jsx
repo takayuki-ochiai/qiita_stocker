869f6f9c22916769fa21c14f2a277f9dccee529e
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-08-02 19:19:28 +0900

    キーワード検索ボックスのデザイン作成

--- app/assets/javascripts/components/stocks/stock_search_field.jsx
+++ app/assets/javascripts/components/stocks/stock_search_field.jsx
@@ -1,3 +1,5 @@
+var FontAwesome = require('react-fontawesome');
+
 var StockSearchField = React.createClass({
   getInitialState() {
     return {
@@ -17,8 +19,7 @@ var StockSearchField = React.createClass({
     return(
       <div>
         <form className="stock-search-field" onSubmit={this.handleSubmit}>
-          <div><input placeholder="ストックした投稿を検索" type="text" ref="keywordQuery" value={this.state.keywordQuery} onChange={this.handleTextChange} /></div>
-          <input type="submit" value="検索する" />
+          <div className="stock-search-field__wrap"><input className="stock-search-field__input" placeholder="ストックした投稿を検索" type="text" ref="keywordQuery" value={this.state.keywordQuery} onChange={this.handleTextChange} /><button className="stock-search-field__submit" type="submit"><FontAwesome name='search' size='lg' /></button></div>
         </form>
       </div>
     )
