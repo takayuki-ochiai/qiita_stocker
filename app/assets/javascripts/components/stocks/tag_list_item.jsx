29b897365c6749ae2dbfa8124e6f013e8679a0ab
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-08-02 12:38:31 +0900

    ストックが持っているタグをクリックするとqiitaのタグのページに飛ぶように修正した

--- app/assets/javascripts/components/stocks/tag_list_item.jsx
+++ app/assets/javascripts/components/stocks/tag_list_item.jsx
@@ -1,10 +1,10 @@
 var TagListItem = React.createClass({
   render() {
     return(
-      <div className="tag-list__item-wrap">
+      <a href={"http://qiita.com/tags/" + this.props.tag.name} className="tag-list__item-wrap" target="_blank">
           <div className="left-arrow"></div>
           <div className="tag-list__item">{this.props.tag.name}</div>
-      </div>
+      </a>
     )
   }
 });
