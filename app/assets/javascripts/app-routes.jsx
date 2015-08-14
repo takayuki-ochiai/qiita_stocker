58a3ad444410d1bbf4a8073071d501dd0968f6af
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-08-14 11:21:39 +0900

    ヘッダーのメニューはいらないので削除した

--- app/assets/javascripts/app-routes.jsx
+++ app/assets/javascripts/app-routes.jsx
@@ -4,9 +4,7 @@ var React = require('react'),
       Route = Router.Route,
       RouteHandler = Router.RouteHandler,
       Index = require('./components/index.jsx'),
-      UserStore = require('./stores/user_store.jsx'),
       FontAwesome = require('react-fontawesome'),
-      ActionCreator = require('./actions/action_creator.js');
 
 var Root = React.createClass({
   render: function() {
@@ -20,32 +18,11 @@ var Root = React.createClass({
 });
 
 var Header = React.createClass({
-  getInitialState() {
-    return {
-      user: {}
-    }
-  },
-  componentDidMount() {
-    UserStore.addChangeListener(this._onUserChange);
-    ActionCreator.fetchUser();
-  },
-  componentWillUnmount() {
-    UserStore.removeChangeListener(this._onUserChange);
-  },
-  _onUserChange() {
-    this.setState({user: UserStore.getAll() })
-  },
   render() {
     return (
       <header>
           <div className="header-container">
               <h1 className="header-container__logo">QiitaSearcher</h1>
-              <div className="header-container__menu">
-                  <div className="header-container__image-wrapper">
-                      <img src={this.state.user.profile_image_url} />
-                      <FontAwesome name="sort-desc" />
-                  </div>
-              </div>
           </div>
       </header>
     )
