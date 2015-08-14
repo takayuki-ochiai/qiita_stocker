498672f5e1c4dc58dc9af9dbe96ee44bbd4f818e
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-08-14 23:45:12 +0900

    JSON.parseの部分をメソッド抽出してリファクタリング

--- app/models/qiita_client.rb
+++ app/models/qiita_client.rb
@@ -3,7 +3,7 @@ class QiitaClient
   require 'uri'
   require 'json'
 
-  API_URI = URI.parse('http://qiita.com/api/v2')
+  API_URI = URI.parse('http://qiita.com/api/v2/users')
   Net::HTTP.version_1_2
 
   def initialize(user_id, token)
@@ -18,7 +18,7 @@ class QiitaClient
     Net::HTTP.start(API_URI.host, API_URI.port) do |http|
       page_num = 1
       while true do
-        stocks_per_page = JSON.parse(http.get("#{API_URI.request_uri}/users/#{@user_id}/stocks?page=#{page_num}&per_page=100", header = {'Authorization' => "Bearer #{@token}"}, dest = nil).body)
+        stocks_per_page = get_json(http, "stocks?page=#{page_num}&per_page=100")
         @stocks.concat(stocks_per_page)
         page_num += 1
         break if stocks_per_page.count < 100
@@ -31,10 +31,14 @@ class QiitaClient
     #qiitaのapiを叩いてフォロー中のタグとユーザーをとってくる
     Net::HTTP.version_1_2
     Net::HTTP.start(API_URI.host, API_URI.port) do |http|
-      @followees = JSON.parse(http.get("#{API_URI.request_uri}/users/#{@user_id}/followees", header = {'Authorization' => "Bearer #{@token}"}, dest = nil).body)
-
-      @following_tags = JSON.parse(http.get("#{API_URI.request_uri}/users/#{@user_id}/following_tags", header = {'Authorization' => "Bearer #{@token}"}, dest = nil).body)
+      @followees = get_json(http, "followees")
+      @following_tags = get_json(http, "following_tags")
     end
     return { followees: @followees, following_tags: @following_tags }
   end
+
+  private
+    def get_json(http, last_uri)
+      JSON.parse(http.get("#{API_URI.request_uri}/#{@user_id}/#{last_uri}", header = {'Authorization' => "Bearer #{@token}"}, dest = nil).body)
+    end
 end
