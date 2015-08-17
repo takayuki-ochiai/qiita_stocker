0762a6541bba9ccd45b1ccc21b12bdbdf59527bf
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-08-17 23:21:34 +0900

    コメント追加　dispatcherとconstants

--- app/assets/javascripts/constants/app_constants.js
+++ app/assets/javascripts/constants/app_constants.js
@@ -1,20 +1,42 @@
+/**
+* @fileoverview QiitaStockerのクライアントサイドで共通で扱う定数を
+*   管理するファイルです。
+* @author takayuki-ochiai
+*/
+
 var Constants = {
+  /** エラー時 */
   ERROR: "error",
+
+  /** イベント発生時 */
   CHANGE_EVENT: 'change',
+
+  /** サインイン確認時に発行する */
   CONFIRM_SIGNIN: 'confirm-signin',
+
+  /** 初期化時に発行する */
   INITIALIZE_STOCKS: 'initialize-stocks',
   INITIALIZE_FILTERS: 'initialize-filters',
   INITIALIZE_USER: 'initialize-user',
   INITIALIZE_STOCK_NUMBER: 'initialize-stock-number',
+
+  /** キーワード保存時に発行する */
   STORE_KEYWORD_QUERY: 'store-keyword-query',
+
+  /** フィルター選択時に発行する */
   TOGGLE_FILTER_OPTION_QUERY: 'toggle-filter-option-query',
+
+  /** クエリ発行 */
   EMIT_QUERY: 'emit-query',
+
+  /** ストックした投稿を読み込み時に発行する*/
   LOAD_STOCKS: 'load-stocks',
 
-  //filter-optionの種類を表す定数
+  /** filter-optionの種類を表す定数 */
   FOLLOWEE_FILTER: 'followee-filter',
   FOLLOWING_TAG_FILTER: 'following-tag-filter',
-  //filter-optionの初期化を表す定数
+
+  /** filter-optionの初期化を表す定数 */
   CLEAR_OPTIONS: 'clear-options'
 };
 
--- app/assets/javascripts/dispatcher/dispatcher.js
+++ app/assets/javascripts/dispatcher/dispatcher.js
@@ -3,6 +3,13 @@ var React            = require('react'),
       assign = require('object-assign');
 
 var AppDispatcher = assign(new Dispatcher(), {
+
+  /**
+  * 命令に合わせたStoreへデータを配送するための
+  * EventEmitterです。
+  * @prams type イベントの種類。app_constantsの持つ定数が入る。
+  * @params action 渡されるデータ。
+  */
   handleViewAction(type, action) {
     this.dispatch({
       actionType: type,
