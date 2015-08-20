1234a1b28220f24fa890a9c0f269f5bdbc718632
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-08-20 21:51:04 +0900

    タイトルと本文によるキーワード検索機能に関するテストを追加

---
+++ spec/stock_query_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+describe StockQuery do
+  describe "#select_stocks" do
+    before do
+      client = QiitaClient.new("takayuki-ochiai", "9cd5f03035b0446c6f7f8f261b91faf9400f31b5")
+      @stocks = client.all_stocks
+    end
+
+    context "キーワードのみの場合" do
+      before do
+        @query = StockQuery.new(@stocks, "Ruby", nil, nil)
+        @keyword = Regexp.new("Ruby", Regexp::IGNORECASE)
+      end
+
+      it "キーワードが含まれるタイトルのものが選択される" do
+        titles = @query.select_stocks
+          .map do |stock|
+            stock["title"]
+          end
+
+        expect(titles).to include("Ruby でベンチマークを取る方法", "RubyでJSON形式の結果が返ってくるURLをParseする", "Rubyで使えるGoogle API一覧", "[初心者向け] RubyやRailsでリファクタリングに使えそうなイディオムとか便利メソッドとか", "[Ruby] ブロックとProcをちゃんと理解する")
+      end
+
+      it "本文にRubyが含まれるものが選ばれる(大文字小文字を問わない)" do
+        ids = @query.select_stocks
+          .map do |stock|
+            stock["id"]
+          end
+
+        expect(ids).to include("ec8e928f69d099b25764", "42193d066bd61c740612", "3d23aa154b483ef611d3", "f2ae316dc8e98b4ff82a", "c5b2aeaf7d67a9ef310a", "bb60ee62c3ee770de2fc", "8121615d4634500a331c")
+      end
+    end
+  end
+end
