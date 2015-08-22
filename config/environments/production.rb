737d1278708d2ddc258ca8b25a75eada523d75a4
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-08-22 13:23:27 +0900

    不要な設定を削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,9 +20,6 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # #本番環境作成用に追加
-  # config.serve_static_assets = true
-
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
