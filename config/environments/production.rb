3c59962eafc0a44457ff56710ea3a1465f400fe9
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-08-22 13:17:36 +0900

    本番環境を作るためにファイルを修正した

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,18 +20,19 @@ Rails.application.configure do
   # NGINX, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  # #本番環境作成用に追加
+  # config.serve_static_assets = true
+
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  #本番環境作成用に追加
-  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
