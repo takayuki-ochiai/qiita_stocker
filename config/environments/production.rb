3e18b55e0e0fd32fcf1efafbf71a7c95ceee5c44
takayuki-ochiai <go.ahead.oti@gmail.com>
2015-08-22 12:17:04 +0900

    本番環境用の設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  #本番環境作成用に追加
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
